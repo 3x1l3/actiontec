59c4a81b24736fa853700ce1b3294d34f42fea99
3x1l3 <chadklassen1@gmail.com>
2016-02-28 16:42:57 -0700

    Code seperation. Curl class. Added sql file for convience of import.

---
+++ curl.php
@@ -0,0 +1,41 @@
+<?php
+
+class Curl {
+
+	public $username;
+	public $password;
+	
+	public $url;
+	
+	public function __construct() {
+	
+				
+	
+	}
+		
+
+	public function run($html = false) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->url);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_COOKIESESSION, false);
+		curl_setopt($ch, CURLOPT_POST, 3);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "inputUserName=" . $this->username . "&inputPassword=" . $this->password . "&nothankyou=1");
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-Language: en-US,en;q=0.5"));
+		$data = curl_exec($ch);
+
+		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+		//return $data;
+		//return ($httpcode>=200 && $httpcode<300) ? $data : false;
+		if ($html) {
+			return nl2br(htmlspecialchars($data));
+		} else {
+			return $data;
+		}
+	}
+
+}
--- index.php
+++ index.php
@@ -7,6 +7,7 @@ mysql_select_db("actiontec");
 
 //::includes 
 require_once('db.php');
+require_once("curl.php");
 
 //::Classes
 $DB = new DB();
@@ -14,38 +15,15 @@ $DB = new DB();
 //::Define the string we are going to look in for passwords.
 $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-={}[]|\\;:<>,./?';
 
-var_dump($DB->SelectWhere('passwords'));
+$curl = new Curl();
+$curl->url = "192.168.1.254/login.cgi";
+
+$return = $curl->run(true);
+
+var_dump($return);
+var_dump(strstr($return, 'index.html?msg=err'));
 
 die();
-
-function getUrlContent($username, $password, $html = false){
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, '192.168.1.254/login.cgi');
-curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-curl_setopt($ch, CURLOPT_COOKIESESSION,false);
-curl_setopt($ch, CURLOPT_POST, 3);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "inputUserName=".$username."&inputPassword=".$password."&nothankyou=1");
-curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept-Language: en-US,en;q=0.5"));
-$data = curl_exec($ch);
-
-$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-//return $data;
-//return ($httpcode>=200 && $httpcode<300) ? $data : false;
-if ($html) {
-	return nl2br(htmlspecialchars($data));
-} else {
-	return $data;
-}
-}
-
-//echo '<script>Navigator.cookieEnabled = false;</script>';
-$return = getUrlContent('admin','n4x');
-
-$failed = strstr($return, 'index.html?msg=err');
 
 
 
